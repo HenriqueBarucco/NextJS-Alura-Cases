494d49649b9839e3caa02a40aa03de7d7c97facc
Henrique Barucco <henrique_barucco@hotmail.com>
2023-03-30 17:41:38 -0300

    feat: Headers

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "../src/components/Link";
 
 export default function FAQPage() {
   return (
     <div>
+      <Head>
+        <title>FAQ - Alura Cases Campanha</title>
+      </Head>
       <h1>Alura Cases - Página de Perguntas FAQ</h1>
       <Link href="/">Ir para a home.</Link>
     </div>
