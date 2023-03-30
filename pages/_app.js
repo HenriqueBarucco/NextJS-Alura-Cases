1dbb462775241b1db2ab2d1ac3b5be91b14d19df
Henrique Barucco <henrique_barucco@hotmail.com>
2023-03-30 16:58:48 -0300

    feat: Estilizando o projeto

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import App from 'next/amp'
+import GlobalStyle from '../src/theme/GlobalStyle';
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export default MyApp;
--- pages/faq.js
+++ pages/faq.js
@@ -3,7 +3,7 @@ import Link from "../src/components/Link";
 export default function FAQPage() {
   return (
     <div>
-      <h1>ALura Cases - Página de Perguntas FAQ</h1>
+      <h1>Alura Cases - Página de Perguntas FAQ</h1>
       <Link href="/">Ir para a home.</Link>
     </div>
   );
